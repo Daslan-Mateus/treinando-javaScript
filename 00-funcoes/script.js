e203235eb4657ed755d22fe98ed8aec78598ec14
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-01 23:28:52 +0000

    funções

---
+++ 00-funcoes/script.js
@@ -0,0 +1,19 @@
+// Crie uma função para verificar se um valor é Truthy
+function ehVerdadeiro(valor) {
+    return Boolean(valor)
+}
+
+// Crie uma função matemática que retorne o perímetro de um quadrado // lembrando: perímetro é a soma dos quatro lados do quadrado
+function perimetro(lado) {
+    return lado = lado + lado + lado + lado
+}
+
+// Crie uma função que retorne o seu nome completo // ela deve possuir os parâmetros: nome e sobrenome
+
+// Crie uma função que verifica se um número é par
+
+// Crie uma função que retorne o tipo de // dado do argumento passado nela (typeof)
+
+// addEventListener é uma função nativa do JavaScript // o primeiro parâmetro é o evento que ocorre e o segundo o Callback // utilize essa função para mostrar no console o seu nome completo // quando o evento 'scroll' ocorrer.
+
+// Corrija o erro abaixo
