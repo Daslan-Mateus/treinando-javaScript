333bd10d962d5d20fda15e80d896d16ee588e676
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-03 23:22:28 +0000

    continuando com objetos

--- 01-objetos/script.js
+++ 01-objetos/script.js
@@ -6,3 +6,33 @@ const dadosPessoais = {
     cpf: '00011122233'
 
 }
+
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dadosPessoais.nomeCompleto = function() {
+        return `${this.nome} ${this.sobrenome}`
+    }
+
+
+// Modifique o valor da propriedade preco para 3000
+
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  carro.preco = 3000;
+
+  // Crie um objeto de um cachorro que represente um labrador, preto com 10 anos, que late ao ver um homem
+  const cachorro = {
+    raca: 'labrador',
+    cor: 'preto',
+    idade: 10,
+    latir(pessoa){
+        if(pessoa === 'homem'){
+            return 'Late'
+        } else {
+            return 'cachorro mudo'
+        }
+    }
+
+  }
