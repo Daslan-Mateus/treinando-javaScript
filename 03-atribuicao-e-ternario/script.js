fce36346b3067c858a2b20276de0cd5c643e65b4
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2023-11-20 01:48:51 +0000

    ternário

---
+++ 03-atribuicao-e-ternario/script.js
@@ -0,0 +1,49 @@
+/*
+// Some 500 ao valor de scroll abaixo,
+// atribuindo o novo valor a scroll
+var scroll = 1000;
+scroll += 500;
+
+// Atribua true para a variável darCredito,
+// caso o cliente possua carro e casa.
+// E false caso o contrário.
+var possuiCarro = true;
+var possuiCasa = false;
+var darCredito;
+
+if(possuiCarro && possuiCasa)
+    darCredito = true;
+    else 
+        darCredito = false;
+*/
+
+// Por qual motivo o código abaixo retorna com erros?
+{
+    var cor = 'preto';
+    const marca = 'Fiat';
+    let portas = 4;
+  }
+  console.log(cor );
+  
+  // Como corrigir o erro abaixo?
+  const dois = 2;
+  function somarDois(x) {
+    
+    return x + dois;
+  }
+  function dividirDois(x) {
+    return x / dois;
+}
+  somarDois(4);   
+  dividirDois(6);
+  
+  // O que fazer para total retornar 500?
+  var numero = 50;
+  
+  for(let numero = 0; numero < 10; numero++) {
+    console.log(numero);
+  }
+  
+  const total = 10 * numero;
+  console.log(`toral: ${total}`);
+  
